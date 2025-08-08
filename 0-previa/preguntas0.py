43b17082d2521d3e4fe8f3203fe22ca773f401de
nicolascirulli01 <nicolascirulli@gmail.com>
2025-08-08 00:52:37 -0300

    preguntas0 hecho

--- 0-previa/preguntas0.py
+++ 0-previa/preguntas0.py
@@ -1,4 +1,4 @@
-# ---
+﻿# ---
 # jupyter:
 #   jupytext:
 #     text_representation:
@@ -35,8 +35,8 @@ respuestas = {}
 
 # %%
 respuestas[(1,0,"Moneda")] = [
-0, # 0. Cara
-0, # 1. Sello
+1/2, # 0. Cara
+1/2, # 1. Sello
 ""
 ]
 
@@ -52,9 +52,9 @@ respuestas[(1,0,"Moneda")] = [
 
 # %%
 respuestas[(1,1,"Cajas")] = [
-0, # 0. Caja 0
-0, # 1. Caja 1
-0, # 2. Caja 2
+1/3, # 0. Caja 0
+1/3, # 1. Caja 1
+1/3, # 2. Caja 2
 0, # 3. Otro lugar
 ""
 ]
@@ -74,10 +74,10 @@ respuestas[(1,1,"Cajas")] = [
 respuestas[(1,2,"Mentir")] = [
 0, # 0. Maxima incertidumbre (entropía)
 0, # 1. Minima incertidumbre (entropía)
-0, # 2. Maxima incertidumbre (entropía) dada la información disponible (restricciones)
+1, # 2. Maxima incertidumbre (entropía) dada la información disponible (restricciones)
 0, # 3. Minima incertidumbre (entropía) dada la información disponible (restricciones)
 0, # 4. Ninguna de las anteriores
-"",
+"Por lo que entiendo la idea de no mentir sería dar una respuesta lo mas vaga posible(máxima incertidumbre) dada la información que uno tiene",
 ]
 
 # %% [markdown]
@@ -99,15 +99,15 @@ respuestas[(1,2,"Mentir")] = [
 # %%
 respuestas[(1,3,"Universos")] = [
 0, # 0. Regalo = 1, Abren = 1
-0, # 1. Regalo = 1, Abren = 2
-0, # 2. Regalo = 1, Abren = 3
+1/4, # 1. Regalo = 1, Abren = 2
+1/4, # 2. Regalo = 1, Abren = 3
 0, # 3. Regalo = 2, Abren = 1
 0, # 4. Regalo = 2, Abren = 2
-0, # 5. Regalo = 2, Abren = 3
+1/4, # 5. Regalo = 2, Abren = 3
 0, # 6. Regalo = 3, Abren = 1
-0, # 7. Regalo = 3, Abren = 2
+1/4, # 7. Regalo = 3, Abren = 2
 0, # 8. Regalo = 3, Abren = 3
-"Justifique brevemente",
+"Si nosotros reservamos la caja 1 entonces la caja 1 no va a ser abierta. Ni la caja con el regalo va a ser abierta. Luego considero que la probabilidad de que esté en la caja 1 y la otra que no vaya a ser abierta es la misma",
 ]
 
 # %% [markdown]
@@ -127,10 +127,10 @@ respuestas[(1,3,"Universos")] = [
 
 # %%
 respuestas[(1,4,"Razonamiento")] = [
-0, # 0. Antes del siglo 17
-0, # 1. Siglo 17
-0, # 2. Siglo 18
-0, # 3. Siglo 19
+1/4, # 0. Antes del siglo 17
+1/4, # 1. Siglo 17
+1/4, # 2. Siglo 18
+1/4, # 3. Siglo 19
 0, # 4. Siglo 20
 0, # 5. Siglo 21
 "",
@@ -152,9 +152,9 @@ respuestas[(1,5,"Independencia")] = [
 0, # 0. 0.9
 0, # 1. 0.1
 0, # 2. 0.0
-0, # 3. 0.2
+1, # 3. 0.2
 0, # 4. Ninguna de las anteriores
-"Justificar brevemente",
+"Como son eventos independientes, su probabilidad conjunta es multiplicar la probabilidad de cada uno de los eventos",
 ]
 
 # %% [markdown]
@@ -196,13 +196,13 @@ respuestas[(1,6,"Marginal")] = [
 0, # 9. P(Y=1): 0.55
 0, # 10. P(Y=1): 0.60
 0, # 11. P(Y=1): 0.65
-0, # 12. P(Y=1): 0.70
+1, # 12. P(Y=1): 0.70
 0, # 13. P(Y=1): 0.75
 0, # 14. P(Y=1): 0.80
 0, # 15. P(Y=1): 0.85
 0, # 16. P(Y=1): 0.90
 0, # 17. Ninguna de las anteriores
-"Justifique brevemente.",
+"Se obtiene haciendo la suma de probabilidad de los eventos que tienen Y=1.",
 ]
 
 
@@ -234,7 +234,7 @@ respuestas[(1,6,"Marginal")] = [
 respuestas[(1,7,"Condicional")] = [
 0, # 0. P(Y=1|X=1): 1/2
 0, # 1. P(Y=1|X=1): 1/3
-0, # 2. P(Y=1|X=1): 2/3
+1, # 2. P(Y=1|X=1): 2/3
 0, # 3. P(Y=1|X=1): 1/4
 0, # 4. P(Y=1|X=1): 2/4
 0, # 5. P(Y=1|X=1): 3/4
@@ -248,7 +248,7 @@ respuestas[(1,7,"Condicional")] = [
 0, # 13. P(Y=1|X=1): 4/6
 0, # 14. P(Y=1|X=1): 5/6
 0, # 15. Ninguna de las anteriores
-"Justifique brevemente.",
+"Por definición de probabilidad condicional, lo calculo como P(X=1, Y=1)/P(X=1).",
 ]
 
 
@@ -263,8 +263,8 @@ respuestas[(1,7,"Condicional")] = [
 
 # %%
 respuestas[(1,8,"Overfitting")] = [
-0, # 0. No
-0, # 1. Si
+1/2, # 0. No
+1/2, # 1. Si
 "",
 ]
 
@@ -278,9 +278,9 @@ respuestas[(1,8,"Overfitting")] = [
 
 # %%
 respuestas[(1,9,"Evaluación")] = [
-0, # 0. No
+1, # 0. No
 0, # 1. Si
-"Justifique brevemente",
+"Dependiendo el tipo de modelo que se haga pueden variar las métricas a las que se les de mayor relevancia",
 ]
 
 # %% [markdown]
@@ -296,12 +296,12 @@ respuestas[(1,9,"Evaluación")] = [
 
 # %%
 respuestas[(1,10,"Predicción")] = [
-0, # 0. El modelo causal correcto nunca puede ser mejor prediciendo que los complejos algoritmos de AI/ML.
-0, # 1. El modelo causal correcto a veces puede ser mejor, y a veces peor, que los complejos algoritmos de AI/ML.
-0, # 2. El modelo causal correcto nunca puede ser peor prediciendo que los complejos algoritmos de AI/ML.
+1/3, # 0. El modelo causal correcto nunca puede ser mejor prediciendo que los complejos algoritmos de AI/ML.
+1/3, # 1. El modelo causal correcto a veces puede ser mejor, y a veces peor, que los complejos algoritmos de AI/ML.
+1/3, # 2. El modelo causal correcto nunca puede ser peor prediciendo que los complejos algoritmos de AI/ML.
 0, # 3. Los modelos causales solo explican, no predicen.
 0, # 4. Ninguna de las anteriores
-"Justifique brevemente",
+"La verdad nose",
 ]
 
 # %% [markdown]
@@ -328,13 +328,13 @@ respuestas[(1,11,"Diversificación")] = [
 0, # 2. Recursos asignados a Cara: 0.2
 0, # 3. Recursos asignados a Cara: 0.3
 0, # 4. Recursos asignados a Cara: 0.4
-0, # 5. Recursos asignados a Cara: 0.5
-0, # 6. Recursos asignados a Cara: 0.6
+1/2, # 5. Recursos asignados a Cara: 0.5
+1/2, # 6. Recursos asignados a Cara: 0.6
 0, # 7. Recursos asignados a Cara: 0.7
 0, # 8. Recursos asignados a Cara: 0.8
 0, # 9. Recursos asignados a Cara: 0.9
 0, # 10. Recursos asignados a Cara: 1.0
-"Justifique brevemente.",
+"Si asignara menos de 0.5 no aprovecharia la ventaja del x3 en caso de que salga cara, y en 0.6 ya arriesgo un poco mas a aprovechar la ventaja del multiplicador pero sin arriesgar demasiado.",
 ]
 
 # %% [markdown]
@@ -348,8 +348,8 @@ respuestas[(1,11,"Diversificación")] = [
 # %%
 respuestas[(1,12,"Apuesta individual")] = [
 0, # 0. No
-0, # 1. Sí
-"Justifique brevemente.",
+1, # 1. Sí
+"Siendo la cantidad de intentos tan alta y la media aritmética mayor a 1 entonces vale la pena apostar.",
 ]
 
 # %% [markdown]
@@ -366,8 +366,8 @@ respuestas[(1,12,"Apuesta individual")] = [
 respuestas[(1,13,"Fondo común")] = [
 0, # 0. Empeora
 0, # 1. Neutro
-0, # 1. Mejora
-"Justifique brevemente.",
+1, # 2. Mejora
+"Porque aumenta la cantidad de muestras, entonces el factor suerte, deberia de reducirse por asi decirlo.",
 ]
 
 # %% [markdown]
@@ -382,10 +382,8 @@ respuestas[(1,13,"Fondo común")] = [
 
 # %%
 respuestas[(1,14,"Tragedia de los comunes")] = [
-0, # 0. Nos conviene aportar al fondo común
-0, # 1. Es indistinto
-0, # 1. Nos conviene dejar de aportar mientras seguimos recibiendo la cuota del fondo común
-"Justifique brevemente.",
+1/4, # 0. Nos conviene aportar al fondo común
+1/2, # 1. Es indistinto
+1/4, # 2. Nos conviene dejar de aportar mientras seguimos recibiendo la cuota del fondo común
+"No le veo diferencias al caso anterior pero no estoy del todo seguro.",
 ]
-
-
